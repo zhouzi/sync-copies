b7ceac86dfadf5ff881de331e9d1866596d68569
Gabin Aureche <hello@gabinaureche.com>
2020-03-29 17:29:46 +0200

    Add cli

---
+++ server/bin.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+const path = require("path");
+const meow = require("meow");
+const serve = require("./serve");
+
+const { input, flags } = meow(
+  `
+    Usage
+        $ sync-copies <folders>
+    
+    Options
+        --match, -m Files to match in the provided folders.
+        --port, -p The port to listen to.
+        --open, -o Open the app in the default browser.
+    
+    Examples
+        $ sync-copies ./packages/package-a/ ./packages/package-b/ --match '*.md'
+`,
+  {
+    flags: {
+      match: {
+        type: "string",
+        alias: "m",
+        default: "*"
+      },
+      port: {
+        type: "string",
+        alias: "p"
+      },
+      open: {
+        type: "boolean",
+        alias: "o",
+        default: false
+      }
+    }
+  }
+);
+
+serve({
+  ...flags,
+  folders: input.map(inputPath => path.resolve(inputPath))
+});
---
+++ server/serve.js
@@ -0,0 +1,107 @@
+const path = require("path");
+const fs = require("fs");
+const getPort = require("get-port");
+const open = require("open");
+const { ApolloServer, gql } = require("apollo-server-express");
+const express = require("express");
+
+async function serve(opts) {
+  const typeDefs = gql`
+    type FileVersion {
+      path: String!
+      content: String
+    }
+
+    type File {
+      basename: String!
+      versions: [FileVersion]
+    }
+
+    type Query {
+      folders: [String]
+      match: String!
+      files: [File]
+    }
+
+    type Mutation {
+      saveFileVersion(basename: String!, content: String!): File
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      folders: () => {
+        return opts.folders;
+      },
+      match: () => {
+        return opts.match;
+      },
+      files: () =>
+        opts.folders
+          .map(folder => fs.readdirSync(folder))
+          .reduce((acc, files) => acc.concat(files), [])
+          .reduce(
+            (acc, file) => (acc.includes(file) ? acc : acc.concat([file])),
+            []
+          )
+          .map(file => ({
+            basename: file
+          }))
+    },
+    Mutation: {
+      saveFileVersion: (_, { basename, content }) => {
+        opts.folders.forEach(folder => {
+          fs.writeFileSync(path.join(folder, basename), content);
+        });
+
+        return {
+          basename,
+          versions: opts.folders.map(folder => ({
+            path: path.join(folder, basename),
+            content
+          }))
+        };
+      }
+    },
+    File: {
+      versions: ({ basename }) => {
+        return opts.folders.map(folder => ({
+          path: path.join(folder, basename),
+          content: null
+        }));
+      }
+    },
+    FileVersion: {
+      content: parent => {
+        if (fs.existsSync(parent.path)) {
+          return fs.readFileSync(parent.path, "utf8");
+        }
+        return null;
+      }
+    }
+  };
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const app = express();
+
+  server.applyMiddleware({ app });
+
+  app.use(express.static(path.join(__dirname, "./public")));
+
+  app.get((req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+  const ports = (opts.port ? [opts.port] : []).concat([3000, 3001, 4000, 1234]);
+  const port = await getPort({ port: ports });
+  const url = `http://localhost:${port}/`;
+  app.listen(3000, () => {
+    console.log(`Now serving app on ${url}`);
+
+    if (opts.open) {
+      open(url);
+    }
+  });
+}
+
+module.exports = serve;
